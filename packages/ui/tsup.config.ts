173ef7a2a94dc0ca1af37c9dd0db29e79e6b33b0
Maksym <max.sharinov@gmail.com>
2023-05-31 03:58:28 +0300

    feat: pre-commit lint

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -3,11 +3,11 @@ import { defineConfig, Options } from 'tsup'
 export default defineConfig((options: Options) => ({
   treeshake: true,
   splitting: true,
-  entry    : ['src/**/*.tsx'],
-  format   : ['esm', 'cjs'],
-  dts      : true,
-  minify   : true,
-  clean    : true,
-  external : ['react'],
+  entry: ['src/**/*.tsx'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  minify: true,
+  clean: true,
+  external: ['react'],
   ...options,
 }))
